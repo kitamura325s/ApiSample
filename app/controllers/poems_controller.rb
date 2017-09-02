201ef9df0f4d66f9e72e4258120647ab81e8478d
Kitamura Saori <kitamura325s@gmail.com>
2017-09-02 07:42:51 +0000

    poem show add

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,17 @@
 class PoemsController < ApplicationController
+  before_action :set_blog, only: [:show]
+  
   def index
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    render json: @poems
+  end
+
+  private
+    def set_blog
+      @poems = Poem.find(params[:id])
+    end
 end
